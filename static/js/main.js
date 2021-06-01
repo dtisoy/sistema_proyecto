a2cde188634684d4016bb10e880c1aa1159edde8
ltisoy <danieltisoy2001@gmail.com>
2021-06-01 09:43:19 -0500

    Alerta antes de eliminar un registro

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,21 @@
 // cerrar los avisos de las acciones de manera automatica
-$(document).ready(function() {
-    // show the alert
-    setTimeout(function() {
-        $(".alert").alert('close');
-    }, 1500);
+$(document).ready(function () {
+  // show the alert
+  setTimeout(function () {
+    $(".alert").alert("close");
+  }, 1500);
 });
+
+// alerta de eliminacion de registros
+const btnDelete = document.querySelectorAll(".btn-delete");
+
+if (btnDelete) {
+  const btnArray = Array.from(btnDelete);
+  btnArray.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (!confirm("¡¡¡ ¿Está seguro de eliminar este registro? !!!")) {
+        e.preventDefault();
+      }
+    });
+  });
+}
